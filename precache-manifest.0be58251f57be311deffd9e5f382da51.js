9fdf837062f0a216ca489f066296652a8a1e83e4
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-08-14 19:06:34 +0300

    Updates

---
+++ precache-manifest.0be58251f57be311deffd9e5f382da51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd816e49e1fe0eee35d9d192702e983",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5876852a3e5c26deb4ca",
+    "url": "./static/css/main.29f8b730.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5876852a3e5c26deb4ca",
+    "url": "./static/js/main.2fcea045.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
